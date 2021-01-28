429ead9dc502d492b906e4a4e3c4b9fc9889bc55
MOMOKOS <mmk.smz.1018@gmail.com>
2021-01-28 22:37:49 +0900

    法人／ログイン追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,16 +7,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password])
   end
 
-  def after_sign_up_path_for(resource)
-    user_path(resource) 
-  end
+  #def after_sign_up_path_for(resource)
+    #user_path(resource) 
+  #end
 
 
-  def after_inactive_sign_up_path_for(resource)
-    user_path(resource) 
-  end
+  #def after_inactive_sign_up_path_for(resource)
+    #user_path(resource) 
+  #end
 
-end
+
 
 
   # before_action :configure_sign_up_params, only: [:create]
@@ -68,13 +68,15 @@ end
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  #The path used after sign up.
+  def after_sign_up_path_for(resource)
+    asdf
+    root_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
 
+end
