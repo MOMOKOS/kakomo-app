9e0b7ba24b293e01c343d22900dc65c861a2aa72
MOMOKOS <mmk.smz.1018@gmail.com>
2021-01-28 22:31:19 +0900

    法人／個人ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    pages_show_path
-  end
+
 
   private
       def sign_in_required
@@ -13,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   protected
       def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
       end
 end
